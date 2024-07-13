8bd5e729afed1b632dfb0c9ad523118b3ee4cacd
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-07-13 22:32:20 +0100

    implement smooth colouring

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -1,8 +1,13 @@
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
+
+#define ONE_SIXTH 0.16666666666666666
+#define TWO_THIRDS 0.6666666666666666
+#define ONE_THIRD 0.3333333333333333
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -32,31 +37,62 @@ void redraw(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
   SDL_DestroyTexture(texture);
 }
 
+float hue2rgb(float p, float q, float t) {
+  if (t < 0) {
+    t += 1;
+  }
+  if (t > 1) {
+    t -= 1;
+  }
+  if (t < ONE_SIXTH) {
+    return p + (q - p) * 6 * t;
+  }
+  if (t < 0.5) {
+    return q;
+  }
+  if (t < TWO_THIRDS) {
+    return p + (q - p) * (TWO_THIRDS - t) * 6;
+  }
+  return p;
+}
+
+void hsl2rgb(float h, float s, float l, uint8_t *dest) {
+  if (s == 0) {
+    dest[0] = 0;
+    dest[1] = 0;
+    dest[2] = 0;
+    return;
+  }
+  float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+  float p = 2 * l - q;
+  dest[0] = hue2rgb(p, q, h + ONE_THIRD) * 255;
+  dest[1] = hue2rgb(p, q, h) * 255;
+  dest[2] = hue2rgb(p, q, h - ONE_THIRD) * 255;
+}
+
 /// Write a pixel to the the pixel array.
-/// \param r The red value.
-/// \param g The green value.
-/// \param b The blue value.
+/// \param H The hue.
+/// \param S The saturation.
+/// \param L The luminance.
 /// \param x The x-coordinate of the pixel.
 /// \param y The y-coordinate of the pixel.
-void writePixel(uint8_t r, uint8_t g, uint8_t b, uint16_t x, uint16_t y) {
+void writePixel(float H, float S, float L, uint16_t x, uint16_t y) {
   int currPixel = 3 * (y * CANVAS_SIZE + x);
-  pixels[currPixel] = r;
-  pixels[currPixel + 1] = g;
-  pixels[currPixel + 2] = b;
+  hsl2rgb(H, S, L, &pixels[currPixel]);
 }
 
 /// Update the pixels array with a pixel's Mandelbrot colour.
 __attribute__((unused)) void drawMandelbrotPixel(uint16_t x, uint16_t y) {
   double iterationRe = 0;
   double iterationIm = 0;
-  double cRe = x / ((float) CANVAS_SIZE / 4) - 2;
-  double cIm = y / ((float) CANVAS_SIZE / 4) - 2;
+  double cRe = (x + 0.5) / ((float) CANVAS_SIZE / 4) - 2;
+  double cIm = (y + 0.5) / ((float) CANVAS_SIZE / 4) - 2;
 
   int numIterations = 0;
 
-  const int MAX_ITERATIONS = 50;
+  const int MAX_ITERATIONS = 100;
 
-  while (((iterationRe < 1 && iterationRe > -1) || (iterationIm < 1 && iterationIm > -1)) &&
+  while (((iterationRe < 10 && iterationRe > -10) || (iterationIm < 10 && iterationIm > -10)) &&
          numIterations < MAX_ITERATIONS) {
     double temp = iterationRe;
     iterationRe = iterationRe * iterationRe - iterationIm * iterationIm + cRe;
@@ -67,17 +103,9 @@ __attribute__((unused)) void drawMandelbrotPixel(uint16_t x, uint16_t y) {
   if (numIterations == MAX_ITERATIONS) {
     writePixel(0, 0, 0, x, y);
   } else {
-    switch (numIterations % 3) {
-      case 0:
-        writePixel(255, 0, 0, x, y);
-        break;
-      case 1:
-        writePixel(0, 255, 0, x, y);
-        break;
-      case 2:
-        writePixel(0, 0, 255, x, y);
-        break;
-    }
+    float betterIterations =
+      ((float) numIterations) - log(log(sqrt(iterationRe * iterationRe + iterationIm * iterationIm))) / M_LN2;
+    writePixel((float) betterIterations / (float) MAX_ITERATIONS, 1, 0.5, x, y);
   }
 }
 
